456d8aee0df6febccc37701b26ab1815619fdfae
Israel Rincon <i.s.r._182@hotmail.com>
2020-02-03 22:24:08 -0600

    Learning to use functions

--- 2_Conditions.py
+++ 2_Conditions.py
@@ -38,3 +38,80 @@ def calculate_storage(filesize):
 print(calculate_storage(1))    # Should be 4096
 print(calculate_storage(4096)) # Should be 4096
 print(calculate_storage(4097)) # Should be 8192
+
+# If condition example 4
+def exam_grade(score):
+	if score > 95:
+		grade = "Top Score"
+	elif score >= 60:
+		grade = "Pass"
+	else:
+		grade = "Fail"
+	return grade
+
+print(exam_grade(65)) # Should be Pass
+print(exam_grade(55)) # Should be Fail
+print(exam_grade(60)) # Should be Pass
+print(exam_grade(95)) # Should be Pass
+print(exam_grade(100)) # Should be Top Score
+print(exam_grade(0)) # Should be Fail
+
+# If condition example 5
+def format_name(first_name, last_name):
+	# code goes here
+	if first_name>"" and last_name >"" :
+	  return "Name: " + first_name +" " + last_name
+	elif first_name>"" and last_name == "":
+	  return "Name: " + first_name 
+	elif first_name=="" and last_name > "":
+	  return "Name: " + last_name 
+	return ""
+
+print(format_name("Ernest", "Hemingway"))
+# Should be "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should be "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should be "Name: Voltaire"
+
+print(format_name("", ""))
+# Should be ""
+
+#If condition example 6
+
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word1) and len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+#f condition example 7
+
+
+def fractional_part(numerator, denominator):
+	# Operate with numerator and denominator to 
+# keep just the fractional part of the quotient
+    fraction =0
+    if  (denominator == 0 or numerator == 0) or (numerator == denominator):
+      return fraction
+    else:
+        fraction=(numerator/ denominator) - (numerator//denominator)
+        return fraction
+ 
+
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
