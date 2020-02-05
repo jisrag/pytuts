3671857608cb2f6381bb63f9a401e925b4e2a19d
Israel Rincon <i.s.r._182@hotmail.com>
2020-02-05 00:34:25 -0600

    Using nested For loops

--- 4_For_Loops.py
+++ 4_For_Loops.py
@@ -12,7 +12,6 @@ def sum_squares(x):
 print(sum_squares(10)) # Should be 285
 
 #For Example 2  Factorial
-
 def factorial(n):
     result = 1
     for i in range(1, n):
@@ -22,3 +21,52 @@ def factorial(n):
 print(factorial(4)) # should return 24
 print(factorial(5)) # should return 120
 
+#For Example 3 Nested For Loops
+teams = ['Dragons', 'Wolves', 'Pandas', 'Unicorns']
+
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team + " vs " + away_team )
+
+#For Example 4 - Validating the list in a for cicle
+
+def is_valid(user):
+    if len(user)>3:
+        return True
+    return False
+
+
+def validate_users(users):
+  for user in users:
+    if is_valid(user):
+      print(user + " is valid")
+    else:
+      print(user + " is invalid")
+
+validate_users(["purplecat", "pap"])
+
+#For Example 5
+def factorialnew(n):
+    result = 1
+    for x in range(1,n):
+        result += result*x
+    return result
+
+for n in range(10):
+    print(n, factorialnew(n))
+
+
+#For Example 6
+def cube(num):
+  result = num**3
+  return result
+  
+
+for n in range(1,11):
+  print(n, cube(n))
+
+#For Example 7
+for n in range(100):
+  if n%7==0:
+    print(n)
